5b23f75bca2a6bd1631643a9418a468bf5812c93
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-09-25 19:02:18 -0500

    Remove config service, cleanup

--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Toast messages
+builder.Services.AddBlazoredToast();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<PdfManager>();
 builder.Services.AddSingleton<TaskManager>();
-builder.Services.AddSingleton<ConfigService>();
 
-// Toast messages
-builder.Services.AddBlazoredToast();
 
 await builder.Build().RunAsync();
