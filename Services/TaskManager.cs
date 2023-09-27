99d47a856fca1b3deca77439732d39cb8b43afee
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-09-26 19:03:35 -0500

    Add task summary and add info util

--- Services/TaskManager.cs
+++ Services/TaskManager.cs
@@ -9,21 +9,26 @@ namespace Pdf_Acc_Toolset.Services
         /// </summary>
         private static readonly List<AccessibilityTask> Tasks = new();
 
+        private static int TasksRan = 0;
+
         public static void RunQueuedTasks()
         {
             Console.WriteLine("Running all tasks");
             foreach (AccessibilityTask task in Tasks)
             {
                 task.Run();
+                TasksRan++;
             }
             Console.WriteLine("Task Queue Complete");
             NotificationUtil.Inform(NotificationType.Success, "Task Queue Complete");
         }
 
-        public static void RunTask(int index) {
+        public static void RunTask(int index)
+        {
             try
             {
                 Tasks[index].Run();
+                TasksRan++;
                 Console.WriteLine("Task at index: " + index + " ran successfully!");
                 NotificationUtil.Inform(NotificationType.Success, "Task ran successfully");
             }
@@ -37,7 +42,7 @@ namespace Pdf_Acc_Toolset.Services
         {
             Tasks.Add(task);
             Console.WriteLine("Task Created!");
-            NotificationUtil.Inform(NotificationType.Success, "Task Created!");
+            NotificationUtil.Inform(NotificationType.Info, "Task Created!");
         }
 
         public static List<AccessibilityTask> GetAccessibilityTasks()
@@ -49,7 +54,8 @@ namespace Pdf_Acc_Toolset.Services
         /// Removes a task with the specified index
         /// </summary>
         /// <param name="index"></param>
-        public static void RemoveTask(int index) {
+        public static void RemoveTask(int index)
+        {
             try {
                 Tasks.RemoveAt(index);
                 Console.WriteLine("Removing task at index: " + index);
@@ -60,11 +66,8 @@ namespace Pdf_Acc_Toolset.Services
             }
         }
 
-        public static void ReOrderTask(int toBeMoved, MoveTask direction) {
-
-        }
-
-        public static void ModifyTaskStatus(int index, bool markComplete) {
+        public static void ModifyTaskStatus(int index, bool markComplete)
+        {
             try
             {
                 Tasks[index].TaskComplete = markComplete;
@@ -73,6 +76,15 @@ namespace Pdf_Acc_Toolset.Services
             {
                 Console.WriteLine("Failed to modify the status of a task at index: " + index);
             }
+        }
+
+        public static int GetTasksRanCounter() {
+            return TasksRan;
+        }
+
+        public static IEnumerable<AccessibilityTask> GetTasksInQueue()
+        {
+            return Tasks.Where(task => task.TaskComplete == false);
         }
     }
 
---
+++ Services/Util/PdfInfoUtil.cs
@@ -0,0 +1,70 @@
+namespace Pdf_Acc_Toolset.Services.Util
+{
+    public static class PdfInfoUtil
+    {
+        /// <summary>
+        /// Returns the title of the PDF.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetTitle()
+        {
+            try {
+                return PdfManager.GetDocument().GetPdfDocument().GetDocumentInfo().GetTitle();
+            } catch(Exception) {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Returns the language of the PDF
+        /// </summary>
+        /// <returns></returns>
+        public static string GetLang()
+        {
+            try {
+                return PdfManager.GetDocument().GetPdfDocument().GetCatalog().GetLang().ToString();
+            } catch(Exception) {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Returns the amount of pages. If the amount cannot be found, returns -1
+        /// </summary>
+        /// <returns></returns>
+        public static int GetPageCount()
+        {
+            try {
+                return PdfManager.GetDocument().GetPdfDocument().GetNumberOfPages();
+            } catch(Exception) {
+                return -1;
+            }
+        }
+
+        /// <summary>
+        /// Returns the version of the PDF
+        /// </summary>
+        /// <returns></returns>
+        public static string GetPdfVersion()
+        {
+            try {
+                return PdfManager.GetDocument().GetPdfDocument().GetPdfVersion().ToString();
+            } catch(Exception) {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Returns the standard of the PDF (A or UA or none)
+        /// </summary>
+        /// <returns></returns>
+        public static string GetPdfStandard()
+        {
+            try {
+                return PdfManager.GetDocument().GetPdfDocument().GetReader().GetPdfAConformanceLevel().GetConformance();
+            } catch(Exception) {
+                return null;
+            }
+        }
+    }
+}
