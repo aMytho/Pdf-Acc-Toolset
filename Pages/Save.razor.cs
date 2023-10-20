fd26f6148f5b1b5d4045740ab1e532f6cfc4ce4f
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-10-20 12:00:28 -0500

    Move save to code-behind, remove unused services

---
+++ Pages/Save.razor.cs
@@ -0,0 +1,141 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Pdf_Acc_Toolset.Services;
+using Pdf_Acc_Toolset.Services.Pdf;
+using Pdf_Acc_Toolset.Services.Util;
+
+namespace Pdf_Acc_Toolset.Pages;
+
+public partial class Save
+{
+    private PdfInfo info = new();
+    private int taskIndex;
+    
+    [Inject]
+    private NavigationManager navigation { get; set; }
+
+    private bool canDownload = false;
+
+    public List<AccessibilityTask> tasks = new();
+    protected override void OnInitialized()
+    {
+        // Get the tasks in the queue
+        tasks = TaskManager.GetAccessibilityTasks();
+
+        // Get the PDF info
+        info.lang = PdfMetadata.GetLang();
+        info.title = PdfMetadata.GetTitle();
+        info.standard = PdfMetadata.GetPdfStandard();
+        info.version = PdfMetadata.GetPdfVersion();
+        info.pageCount = PdfMetadata.GetPageCount();
+        info.tasksRan = TaskManager.GetTasksRanCounter();
+        info.tasksInQueue = TaskManager.GetTasksInQueue().Count();
+        info.filename = PdfManager.filename;
+
+        // Correct any blank or invalid fields
+        info = AddPdfInfoDefaults(info);
+
+        // Allow/disallow downloads
+        canDownload = PdfManager.pdfDownloadable;
+    }
+
+    private PdfInfo AddPdfInfoDefaults(PdfInfo info)
+    {
+        if (info.title == null || info.title.Length == 0) {
+            info.title = "None";
+        }
+
+        if (info.standard == null || info.standard.Length == 0) {
+            info.standard = "None";
+        }
+
+        if (info.lang == null || info.lang.Length == 0) {
+            info.lang = "None";
+        }
+
+        Console.WriteLine(info);
+
+        return info;
+    }
+
+    private async void SavePdf()
+    {
+        // If the user already downloaded the PDF, stop them from trying again
+        if (!canDownload) {
+            NotificationUtil.Inform(
+                NotificationType.Error, "You cannot download a PDF once it has been saved."
+            );
+            return;
+        }
+
+        // Save the PDF
+        PdfManager.Save();
+        Console.WriteLine("Saving the PDF");
+        // Download the PDF
+        await DownloadPdf();
+
+        // Show the complete screen
+        navigation.NavigateTo("/complete");
+    }
+
+    private void RunQueueAndSavePdf()
+    {
+        if (canDownload) {
+            // Run the queued tasks
+            TaskManager.RunQueuedTasks();
+            // Save the PDF
+            SavePdf();
+        } else {
+            NotificationUtil.Inform(
+                NotificationType.Error, "You cannot download a PDF once it has been saved."
+            );
+        }
+    }
+
+    private async Task DownloadPdf()
+    {
+        // Generate a file
+        byte[] data = PdfManager.outFile.ToArray();
+        Console.WriteLine("Exporting PDF...");
+        // Send the data to JS to actually download the file
+        await JSRuntime.InvokeVoidAsync(
+            "BlazorDownloadFile", CorrectFilename(PdfManager.filename),
+            "application/octet-stream", data
+        );
+        NotificationUtil.Inform(NotificationType.Success, "Starting Export...");
+
+        // Reset all PDF data
+        PdfManager.Close();
+        // Remove any tasks
+        TaskManager.RemoveTasks();
+    }
+
+    // Handle bad filenames
+    private string CorrectFilename(string filename)
+    {
+        // No file name
+        if (filename == null) {
+            return "Default_Name.pdf";
+        }
+
+        // No file extension
+        if (!filename.EndsWith(".pdf")) {
+            return filename + ".pdf";
+        }
+
+        // Good user, good boy!
+        return filename;
+    }
+
+    private struct PdfInfo
+    {
+        public string filename;
+        public string version;
+        public string title;
+        public string standard;
+        public string lang;
+        public int tasksRan;
+        public int tasksInQueue;
+        public int pageCount;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Pdf_Acc_Toolset;
 using Pdf_Acc_Toolset.Services;
-using Pdf_Acc_Toolset.Services.UI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,15 +11,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // Toast messages
 builder.Services.AddBlazoredToast();
 
-// Add TOC for documentation
-builder.Services.AddSingleton<TocService>();
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<PdfManager>();
-builder.Services.AddSingleton<TaskManager>();
-
 // Set the Env
 Config.Env = builder.HostEnvironment.Environment;
-Console.WriteLine("Env is " + Config.Env);
+Console.WriteLine($"Env is {Config.Env}");
 
 await builder.Build().RunAsync();
