644d2ca35a6ec8c0fd9c8b74a15ec96a869ca282
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-11-16 15:39:38 -0600

    Add pre-rendering for SEO

--- Program.cs
+++ Program.cs
@@ -9,11 +9,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Toast messages
-builder.Services.AddBlazoredToast();
+ConfigureServices(builder.Services, builder.HostEnvironment);
 
 // Set the Env
 Config.Env = builder.HostEnvironment.Environment;
 Console.WriteLine($"Env is {Config.Env}");
 
 await builder.Build().RunAsync();
+
+// All Services must be added in this method for pre-rendering
+static void ConfigureServices(IServiceCollection services, IWebAssemblyHostEnvironment hostEnv) {
+    // Toast messages
+    services.AddBlazoredToast();
+}
