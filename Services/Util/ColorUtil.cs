41750807ce7b066b7dd33fa366488d972d3cfc7d
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-12-15 19:06:52 -0600

    Show target color when selected

--- Services/Util/ColorUtil.cs
+++ Services/Util/ColorUtil.cs
@@ -5,7 +5,7 @@ namespace Pdf_Acc_Toolset.Services.Util;
 
 public static class ColorUtil
 {
-    public static float[] convertColorToRgb(Color color)
+    public static float[] ConvertColorToRgb(Color color)
     {
         // If color is already RGB, return it.
         if (color is DeviceRgb)
@@ -17,7 +17,8 @@ public static class ColorUtil
         if (color is DeviceCmyk)
         {
             return ConvertCmykToRgb(color);
-        } else
+        }
+        else
         {
             // Check for gray
             return ConvertGrayToRgb(color);
@@ -37,7 +38,7 @@ public static class ColorUtil
         float red = 255 * (1 - cyan) * (1 - black);
         float green = 255 * (1 - magenta) * (1 - black);
         float blue = 255 * (1 - yellow) * (1 - black);
-        
+
         // return result
         return [red, green, blue];
     }
@@ -50,5 +51,22 @@ public static class ColorUtil
         // // Convert Gray to RGB
         // float result = (float)Math.Round(255 * (1 - grayValue));
         return [grayValue, grayValue, grayValue];
+    }
+
+    public static string ConvertToHex(Color color)
+    {
+        try {
+            float[] rgb = ConvertColorToRgb(color);
+            // Get the individual RGB components
+            int red = (int)(rgb[0] * 255);
+            int green = (int)(rgb[1] * 255);
+            int blue = (int)(rgb[2] * 255);
+
+            // Convert each component to hexadecimal format
+            return $"#{red:X2}{green:X2}{blue:X2}";
+        } catch (Exception) {
+            // If anything breaks, show black as the default
+            return "#000000";
+        }
     }
 }
